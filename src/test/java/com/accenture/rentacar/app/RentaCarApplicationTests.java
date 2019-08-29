0bf72a1da877bc6dff700812b1f0b8ac310ddb02
technology <technology@CPX-HWKFR1UHONJ.dir.svc.accenture.com>
2019-08-28 19:02:21 -0500

    mi primer commit

---
+++ src/test/java/com/accenture/rentacar/app/RentaCarApplicationTests.java
@@ -0,0 +1,16 @@
+package com.accenture.rentacar.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RentaCarApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
