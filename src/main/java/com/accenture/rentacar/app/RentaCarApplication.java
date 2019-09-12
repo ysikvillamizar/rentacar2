ad4d72167f2deaabb85fe270bf0f2bb08f2fe9d8
technology <technology@CPX-HWKFR1UHONJ.dir.svc.accenture.com>
2019-09-11 20:31:02 -0500

    main class

--- src/main/java/com/accenture/rentacar/app/RentaCarApplication.java
+++ src/main/java/com/accenture/rentacar/app/RentaCarApplication.java
@@ -1,15 +1,21 @@
 package com.accenture.rentacar.app;
+import java.util.Date;
 import java.util.Scanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.accenture.uilidades.fechas.Fecha;
+
 @SpringBootApplication
 public class RentaCarApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RentaCarApplication.class, args);
+		//SpringApplication.run(RentaCarApplication.class, args);//esta linea inicia o arranca nuestro proyecto
+		//click derecho run as java application
 		
+		System.out.println("Resultado: " + Fecha.calcularDias(new Date("12/05/2018"), null));//mi error exception
+		//System.out.println("Resultado: " + Fecha.calcularDias(new Date("12/05/2018"), new Date()));
 	        
 	      
 	}
